c5bdab901430a94a1d3379b623adbb7b9b01f700
danielstallworthmit <danielstallworthmit@gmail.com>
2017-09-02 20:48:19 -0700

    Set up components with connect to redux state for book list and selected book.

---
+++ booklist/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        {title: 'Javascript, the Good Parts', pages: 100},
+        {title: 'Harry Potter', pages: 1233},
+        {title: 'Darkness Holds', pages: 54},
+        {title: 'Ruby, the Great Parts', pages: 150}
+    ];
+}
